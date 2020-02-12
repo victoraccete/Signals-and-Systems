22c0acebb2f5f5a0af710c0f8ea948d3586a25ae
Victor Accete <victor.accete@gmail.com>
2020-02-12 00:51:04 -0300

    computing dfts

--- DFT.m
+++ DFT.m
@@ -7,12 +7,13 @@ t = (0:L-1)*T;        % Time vector
 % Uma de amplitude 0.7 e frequência de 50 Hz
 % e a outra de amplitude 1 e frequência de 120 Hz
 S = 0.7*sin(2*pi*50*t) + sin(2*pi*120*t);
-
+S2 = 0.7*sin(2*pi*50*t);
 % colocar para plotar os sinais com subplot
 
 % Corrompendo um sinal com um ruído
 X = S + 2*randn(size(t));
 
+figure(1)
 plot(1000*t(1:50),X(1:50))
 title('Sinal corrompido com ruído')
 xlabel('t (em milissegundos)')
@@ -20,4 +21,28 @@ ylabel('X(t)')
 
 % calculando a transformada de Fourier do sinal X
 Y = fft(X);
-% continua https://www.mathworks.com/help/matlab/ref/fft.html
+
+% computando o espectro bilateral
+P2 = abs(Y/L);
+% calculando o espectro unilateral a partir do bilateral
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+f = Fs*(0:(L/2))/L;
+figure(2)
+plot(f,P1) 
+title('Amplitude a partir do sinal corrompido')
+xlabel('f (Hz)')
+ylabel('Amplitude')
+
+% agora, plotando a transformada mas para o sinal incorrompido por ruídos
+figure(3)
+Y = fft(S);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+plot(f,P1) 
+title('Amplitude a partir do sinal original')
+xlabel('f (Hz)')
+ylabel('Amplitude')
